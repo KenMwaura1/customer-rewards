65eef0200b7315da5df8f645af6a03210a57666e
KenMwaura1 <kemwaura@gmail.com>
2021-04-05 21:49:54 +0300

    first commit

--- customer_search.py
+++ customer_search.py
@@ -1,7 +1,19 @@
-from database_insert import engine, Customers
+from config import engine
+from database_insert import Customers, Sales_Transaction
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from datetime import date
+
 session = Session(engine, future=True)
-test = select(Customers).all()
+test = select(Customers)
 new = session.execute(test).all()
-print(new)
+t2 = session.query(Customers.customer_id, Customers.customer_first_name).all()
+for x, y in t2:
+    print(x, y)
+dt = date(2021, 4, 4)
+print(dt)
+sq = session.query(Sales_Transaction.transaction_id, Sales_Transaction.customer_id,
+                   Sales_Transaction.transaction_price, Sales_Transaction.transaction_date)
+for a, b, c, d in sq.filter(Sales_Transaction.transaction_price > 1200)\
+        .filter(Sales_Transaction.transaction_date > dt ):
+    print(a, b, c, d)
--- database_insert.py
+++ database_insert.py
@@ -12,7 +12,6 @@ load_dotenv()
 
 at_username = os.getenv('at_username')
 at_api_key = os.getenv('at_api_key')
-)
 
 
 class Customers(Base):
@@ -47,8 +46,10 @@ c2 = Customers(customer_id=10, customer_first_name='amy',
                _phonenumber=temp.phone_number.e164)
 s2 = Sales_Transaction(customer_id=c1.customer_id,
                        transaction_price=2700, transaction_date=dt.datetime.utcnow())
+""" 
 s.add(c1)
 s.add(c2)
 s.add(s2)
 s.commit()
 s.close()
+"""
