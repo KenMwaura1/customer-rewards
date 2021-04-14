33de8970158c23635e7b63869fa1a8631d437b63
KenMwaura1 <kemwaura@gmail.com>
2021-04-14 22:20:49 +0300

    added try-catch block

--- database_insert.py
+++ database_insert.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 from config import engine, Base, session
 
-phone_number = '0719702373'
+phone_number = '0729450130'
 
 
 class Customers(Base):
@@ -31,17 +31,19 @@ class Sales_Transaction(Base):
 Base.metadata.create_all(engine)
 
 temp = Customers(phone_number=PhoneNumber(f'{phone_number}', 'KE'))
-c1 = Customers(customer_id=2, customer_first_name='ken',
+c1 = Customers(customer_id=43, customer_first_name='ken',
                customer_last_name='mwaura', _phonenumber=temp.phone_number.e164)
-c2 = Customers(customer_id=3, customer_first_name='zoo',
-               customer_last_name='mwaura',
+c2 = Customers(customer_id=37, customer_first_name='babygirl',
+               customer_last_name='nyambura',
                _phonenumber=temp.phone_number.e164)
 s2 = Sales_Transaction(customer_id=c1.customer_id,
-                       transaction_price=5000, transaction_date=dt.date(2021, 4, 10))
-s3 = Sales_Transaction(customer_id=c2.customer_id, transaction_price=2500,
-                       transaction_date=dt.date(2021, 4, 10))
-
-session.add_all([c1, c2,])
-session.add_all([s2, s3])
-session.commit()
-session.close()
+                       transaction_price=3850, transaction_date=dt.date(2021, 4, 10))
+s3 = Sales_Transaction(customer_id=c2.customer_id, transaction_price=7000,
+                       transaction_date=dt.date(2021, 4, 18))
+try:
+    session.add_all([c1, c2,])
+    session.add_all([s2, s3])
+    session.commit()
+    session.close()
+except Exception as e:
+    print(f"Exception occurred: {e}")
